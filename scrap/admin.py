f5da180492def390f12695f67d6c469ca0412431
Alex <lescot.alexg@gmail.com>
2023-01-29 17:47:17 +0100

    upgrade interface admin

--- scrap/admin.py
+++ scrap/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from .models import Categorie, Document
 
-admin.site.register(Categorie)
-admin.site.register(Document)
+from django.contrib import admin
+from .models import Document, Categorie
+
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ('title', 'categorie')
+
+    list_filter = ('categorie',)
+
+class CategorieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'corrections')
+
+admin.site.register(Document, DocumentAdmin)
+admin.site.register(Categorie, CategorieAdmin)
 
 # git reset --hard HEAD
