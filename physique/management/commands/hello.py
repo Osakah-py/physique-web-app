e27e633d6ae54ac76d45eef0bf84a7a3456d8f5c
Alex <lescot.alexg@gmail.com>
2023-02-26 21:54:38 +0100

    rename de l'appli scrap en physique (quel bordel)

---
+++ physique/management/commands/hello.py
@@ -0,0 +1,23 @@
+from django.core.management.base import BaseCommand, CommandError
+from physique.models import Categorie, Document
+
+class Command(BaseCommand):
+    args = ''
+    help = 'affiche Hello worlds !'
+    
+    def handle(self, *args, **options):
+        documents_by_category = {}
+        correction_by_category ={}
+
+        categories = Categorie.objects.order_by('pk')
+        for category in categories:
+            if category.corrections:
+                correction = Document.objects.filter(categorie=category, title__icontains='correction')
+                correction_by_category[category] = correction
+                documents = Document.objects.filter(categorie=category).exclude(title__icontains='correction')
+                documents_by_category[category] = documents
+            else:
+                documents = Document.objects.filter(categorie = category)
+                documents_by_category[category] = documents
+        print(documents_by_category)
+        print(correction_by_category)
