7a2469c7759d9d8370203f16ad9c61934caf9e80
Osakah-py <lescot.alexg@gmail.com>
2023-10-18 17:38:26 +0000

    ajout de logs pour mieux analyser la commande hebdomadaire de reset de stats

--- physique/management/commands/stats_reset.py
+++ physique/management/commands/stats_reset.py
@@ -5,10 +5,11 @@ import datetime
 class Command(BaseCommand):
     args = ''
     help = 'reset les enregistrements de statistiques pour la semaine en cours'
-    
+
     def handle(self, *args, **options):
         today = datetime.date.today()
         weekday = today.weekday()
+        print("Nous sommes le " + str(weekday) + "eme jour de la semaine", flush=True)
         if (weekday == 0):
              # On reset la stat de semaine
              tout = Statistiques.objects.all()
@@ -16,5 +17,6 @@ class Command(BaseCommand):
                  stat.telechargements_semaine_derniere = stat.telechargements_cette_semaine
                  stat.telechargements_cette_semaine = 0
                  stat.save()
+             print("-> full reset terminé", flush=True)
         else:
-            print("rien aujourd'hui")
+            print("-> rien aujourd'hui", flush=True)
