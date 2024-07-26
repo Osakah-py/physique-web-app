98095b348f2c0da474d172e561b58690aa4bea98
alex <lescot.alexg@gmail.com>
2024-07-26 16:53:48 +0200

    ajout des options admin pour visible chimie

--- chimie/admin.py
+++ chimie/admin.py
@@ -1,5 +1,40 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.utils.translation import ngettext
 from .models import Categorie, Document
 
-admin.site.register(Document)
+# DOCUMENT MODEL 
+@admin.action(description="Rendre visible")
+def make_visible(modeladmin, request, queryset):
+    updated = queryset.update(visible=True)
+    modeladmin.message_user(
+            request,
+            ngettext(
+                "%d document est désormais visible.",
+                "Les %d documents sont désormais visibles.",
+                updated,
+            )
+            % updated,
+            messages.SUCCESS,
+        )
+
+@admin.action(description="Masquer")
+def make_hidden(modeladmin, request, queryset):
+    updated = queryset.update(visible=False)
+    modeladmin.message_user(
+            request,
+            ngettext(
+                "%d document est désormais masqué.",
+                "Les %d documents sont désormais masqués.",
+                updated,
+            )
+            % updated,
+            messages.SUCCESS,
+        )
+
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ('title', 'categorie', 'visible')
+    list_filter = ('categorie',)
+    actions = [make_visible, make_hidden]
+
+admin.site.register(Document, DocumentAdmin)
 admin.site.register(Categorie)
