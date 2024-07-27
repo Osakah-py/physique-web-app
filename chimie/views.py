54ef8eae7f65635154f72cc8ca6e1b125a53110e
alex <lescot.alexg@gmail.com>
2024-07-27 15:35:31 +0200

    correction bug décalage lors de l'attribution document/correction

--- chimie/views.py
+++ chimie/views.py
@@ -9,24 +9,22 @@ def main(request):
 
     for category in categories:
         if request.user.is_authenticated and admin_view:
-            correction = Document.objects.filter(categorie=category, title__icontains='correction').order_by('-pk')
-            documents = Document.objects.filter(categorie=category).exclude(title__icontains='correction').order_by('-pk')
+            correction = list(Document.objects.filter(categorie=category, title__icontains='correction').order_by('-pk'))
+            documents = list(Document.objects.filter(categorie=category).exclude(title__icontains='correction').order_by('-pk'))
         else: 
-            correction = Document.objects.filter(categorie=category, visible=True, title__icontains='correction').order_by('-pk')
-            documents = Document.objects.filter(categorie=category, visible = True).exclude(title__icontains='correction').order_by('-pk')
+            correction = list(Document.objects.filter(categorie=category, visible=True, title__icontains='correction').order_by('-pk'))
+            documents = list(Document.objects.filter(categorie=category, visible = True).exclude(title__icontains='correction').order_by('-pk'))
         
-        l_doc = len(documents)
-        l_corr = len(correction)
-        c = 0
         documents_by_category[category] = []
         
-        for i in range(l_doc):
-            if c >= l_corr: 
-                documents_by_category[category].append((documents[i], "N/A"))
-            elif "bonus" in documents[i].title or ("14" in documents[i].title and not("14" in correction[i].title)):
-                documents_by_category[category].append((documents[i], "N/A"))
-            else : 
-                documents_by_category[category].append((documents[i], correction[c]))
-                c += 1
+        while documents != []:
+            curr_doc = documents.pop()
+            if correction == []:
+                documents_by_category[category].append((curr_doc, "N/A"))
+            else:
+                curr_corr = correction.pop()
+                documents_by_category[category].append((curr_doc, curr_corr))
+
+        documents_by_category[category] = documents_by_category[category][::-1]
             
     return render(request, 'main.html', {'documents_by_category': documents_by_category, 'admin_view':admin_view})
